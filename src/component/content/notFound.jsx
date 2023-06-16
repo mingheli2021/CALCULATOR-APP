971c379e392058db28c2ae69cf8fa01d20af632c
Kyle Li <kyleyli2005@gmail.com>
2023-06-15 21:06:56 -0700

    basic structure

---
+++ src/component/content/notFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Base from './base';
+
+class NotFound extends Component {
+    state = {}
+    render() {
+        return (
+            <Base>NotFound</Base>
+        );
+    }
+}
+
+export default NotFound;
---
+++ src/component/content/register.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Base from './base';
+
+class Register extends Component {
+    state = {}
+    render() {
+        return (
+            <Base>Register</Base>
+        );
+    }
+}
+
+export default Register;
